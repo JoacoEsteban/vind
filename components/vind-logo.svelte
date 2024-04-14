5ea1806f273ffe9eba79d896e34fe096d67981f9
Joaco Esteban <dev.joaco.esteban@gmail.com>
2024-04-15 01:23:10 +0200

    Add Vind logo component

---
+++ components/vind-logo.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import logo from '~/assets/icon.png'
+
+  export let size: string = '100px'
+</script>
+
+<img src={logo} alt="Vind logo" style:width={size} style:height={size} />
