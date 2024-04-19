ba543846451765934f574fe709018e900eb0fde3
Joaco Esteban <dev.joaco.esteban@gmail.com>
2024-04-19 15:58:35 +0200

    Add arrowTriangleBranch and link symbols

--- lib/symbols.ts
+++ lib/symbols.ts
@@ -5,6 +5,8 @@ import arrowDownRaw from '~/assets/symbols/arrow.down.json'
 import xMarkRaw from '~/assets/symbols/xmark.json'
 import trashFillRaw from '~/assets/symbols/trash.fill.json'
 import sidebarRightRaw from '~/assets/symbols/sidebar.right.json'
+import arrowTriangleBranchRaw from '~/assets/symbols/arrow.triangle.branch.json'
+import linkRaw from '~/assets/symbols/link.json'
 
 export const SymbolSchema = Record({
   path: String,
@@ -25,12 +27,16 @@ const [
   xMark,
   trashFill,
   sidebarRight,
+  arrowTriangleBranch,
+  link,
 ] = [
   gearRaw,
   arrowDownRaw,
   xMarkRaw,
   trashFillRaw,
   sidebarRightRaw,
+  arrowTriangleBranchRaw,
+  linkRaw,
 ]
   .map(checkSymbol)
   .map(symbol => {
@@ -56,4 +62,8 @@ export const symbols = {
   xMark,
   trashFill,
   sidebarRight,
+  arrowTriangleBranch,
+  link,
 }
+
+export type symbolName = keyof typeof symbols
