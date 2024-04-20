83e76d9a66d0e32aef1e32769fd9cb01d80d10ff
Joaco Esteban <dev.joaco.esteban@gmail.com>
2024-04-20 13:26:35 +0200

    FEAT, implement commands and add support for toggle-overlay command

--- background/index.ts
+++ background/index.ts
@@ -7,12 +7,21 @@ import { PageOverridesStorageImpl } from './storage/page-overrides-storage'
 import { bindingsMessages } from '~messages/storage'
 import { log } from '~lib/log'
 import { StorageHandlers } from './handlers'
+import { match } from 'ts-pattern'
 
 const db = new VindDB()
 export const bindingsStorage = new BindingsStorageImpl(db)
 export const pageOverridesStorage = new PageOverridesStorageImpl(db)
 
 new StorageHandlers(bindingsStorage, pageOverridesStorage).init()
+
+async function sendShowOverlay () {
+  const tabId = await getActiveTabId()
+  if (!tabId) return
+  showOverlay.toTab({
+    tabId: tabId
+  })
+}
 
 chrome.tabs.onActivated.addListener(activeInfo => {
   wakeUp.ask.toTab({
@@ -32,11 +41,16 @@ askForOptionsPageStream.subscribe(async ([, sender]) => {
   chrome.runtime.openOptionsPage()
 })
 
+chrome.commands.onCommand.addListener(async (command) => {
+  log.info('Command received', command)
+  match(command)
+    .with('toggle-overlay', sendShowOverlay)
+    .otherwise(() => {
+      log.warn('No matching command found for', `"${command}"`)
+    })
+})
+
 chrome.action.onClicked.addListener(async (tab) => {
   log.info('Action clicked')
-  const tabId = await getActiveTabId()
-  if (!tabId) return
-  showOverlay.toTab({
-    tabId: tabId
-  })
+  sendShowOverlay()
 })
