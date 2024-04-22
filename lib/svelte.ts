356ba7fdb180a4735636828dbd7816a6a3e80d0a
Joaco Esteban <dev.joaco.esteban@gmail.com>
2024-04-22 13:57:26 +0200

    Add wrapIterable function to lib/svelte.ts

---
+++ lib/svelte.ts
@@ -0,0 +1,29 @@
+type ContextfulIteration<T> = {
+  item: T
+  index: number
+  first: boolean
+  last: boolean
+  odd: boolean
+  even: boolean
+}
+
+export function wrapIterable<T> (iter: Iterable<T>): Iterable<ContextfulIteration<T>> {
+  return {
+    [Symbol.iterator]: function* () {
+      const arr = Array.from(iter)
+      for (let i = 0; i < arr.length; i++) {
+        const even = i % 2 === 0
+        const odd = !even
+
+        yield {
+          item: arr[i],
+          index: i,
+          first: i === 0,
+          last: i === arr.length - 1,
+          odd,
+          even,
+        }
+      }
+    }
+  }
+}
