68b2c363e02143ba03d8cb5ed3a1cacd36f9056b
Joaco Esteban <dev.joaco.esteban@gmail.com>
2024-04-25 19:01:37 +0200

    Update imports and add SymbolComponent constructor

--- lib/symbols.ts
+++ lib/symbols.ts
@@ -1,4 +1,4 @@
-import { Record, String, Number, type Static } from 'runtypes'
+import { Record as RTRecord, String, Number, type Static } from 'runtypes'
 
 import gearRaw from '~/assets/symbols/gear.json'
 import arrowDownRaw from '~/assets/symbols/arrow.down.json'
@@ -7,10 +7,12 @@ import trashFillRaw from '~/assets/symbols/trash.fill.json'
 import sidebarRightRaw from '~/assets/symbols/sidebar.right.json'
 import arrowTriangleBranchRaw from '~/assets/symbols/arrow.triangle.branch.json'
 import linkRaw from '~/assets/symbols/link.json'
+import { default as SvelteSymbolComponent } from '~components/symbol.svelte'
+import type { ComponentConstructorOptions } from 'svelte'
 
-export const SymbolSchema = Record({
+export const SymbolSchema = RTRecord({
   path: String,
-  geometry: Record({
+  geometry: RTRecord({
     width: Number,
     height: Number,
   })
@@ -65,5 +67,27 @@ export const symbols = {
   arrowTriangleBranch,
   link,
 }
-
 export type SymbolName = keyof typeof symbols
+
+export function SymbolComponent (name: SymbolName, size: string = '100%') {
+  return class extends SvelteSymbolComponent {
+    constructor(arg: ComponentConstructorOptions<typeof SvelteSymbolComponent>) {
+      const props = {
+        ...arg.props,
+        name,
+        size
+      }
+
+      super({
+        ...arg,
+        props,
+      })
+    }
+  } as typeof SvelteSymbolComponent
+}
+
+export const SymbolComponents: Record<SymbolName, typeof SvelteSymbolComponent> = (Object.keys(symbols) as SymbolName[])
+  .reduce((obj, name) => {
+    obj[name as SymbolName] = SymbolComponent(name)
+    return obj
+  }, {} as Record<SymbolName, typeof SvelteSymbolComponent>)
