427f2575370594115abe375fe5971ff13980598c
Joaco Esteban <dev.joaco.esteban@gmail.com>
2024-04-28 21:17:10 +0200

    Add debug logging to log module

--- lib/log.ts
+++ lib/log.ts
@@ -26,6 +26,7 @@ export const log = {
   error: wrap(console.error, 'ERROR', 'red'),
   info: wrap(console.info, 'INFO', 'cyan'),
   log: wrap(console.log, 'LOG', ''),
+  debug: wrap(console.log, 'DEBUG', 'violet'),
   warn: wrap(console.warn, 'WARN', 'orange'),
   success: wrap(console.log, 'SUCCESS', '#0f0'),
 }
