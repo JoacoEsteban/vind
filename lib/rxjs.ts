569da9b1e4114e20df3099cc47134161c25e6b73
Joaco Esteban <dev.joaco.esteban@gmail.com>
2024-04-29 00:22:30 +0200

    Add Stack functionality to rxjs.ts

--- lib/rxjs.ts
+++ lib/rxjs.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, Subject, type Observable } from 'rxjs'
+import { BehaviorSubject, Subject, type Observable, map, queue } from 'rxjs'
 
 export function expose<T> (observable: Observable<T>) {
   const subject = new BehaviorSubject<T | null>(null)
@@ -27,5 +27,27 @@ export function PromiseStopper (promise: Promise<any>) {
   return {
     stopper,
     stop
+  }
+}
+
+export function Stack () {
+  const stack$$ = new BehaviorSubject<number>(0)
+
+  return {
+    stack$$,
+    empty$: stack$$.pipe(map((val) => !val)),
+    full$: stack$$.pipe(map((val) => val > 0)),
+    push: function () {
+      stack$$.next(stack$$.value + 1)
+    },
+    pop: function () {
+      stack$$.next(stack$$.value - 1)
+    },
+    reset: function () {
+      stack$$.next(0)
+    },
+    complete: function () {
+      stack$$.complete()
+    }
   }
 }
