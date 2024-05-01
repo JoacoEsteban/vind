8a854c3cbefec1413bf66c5557b85bcf33a5a857
Joaco Esteban <dev.joaco.esteban@gmail.com>
2024-05-01 18:20:11 +0200

    FEAT, add MapToOrderedTuple return type

--- lib/map.ts
+++ lib/map.ts
@@ -1,3 +1,3 @@
 export function MapToOrderedTuple<T, K> (map: Map<T, K>, order: (a: T, b: T) => number) {
-  return Array.from(map.entries()).sort(([a], [b]) => order(a, b))
+  return Array.from(map.entries()).sort(([a], [b]) => order(a, b)) as [T, K][]
 }
