fe5cbd838209d530f20a70634d65c001910fce8d
Joaquin Esteban <dev@joacoesteban.com>
2024-05-16 14:57:45 +0200

    Add functions to get extension version and check if two versions have the same major

--- lib/misc.ts
+++ lib/misc.ts
@@ -1,3 +1,14 @@
+import semver from 'semver'
+import { interopRuntime } from '~background/utils/runtime'
+
+export function getExtensionVersion () {
+  return interopRuntime().getManifest().version
+}
+
+export function areSameMajor (a: string, b: string) {
+  return semver.major(a) === semver.major(b)
+}
+
 export function openGithub () {
   window.open('https://github.com/joacoesteban/vind')
 }
