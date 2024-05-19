adc75a6fbee551c4863c510ac51593e54b68bb72
Joaquin Esteban <dev@joacoesteban.com>
2024-05-19 23:49:09 +0200

    Add Vind logo and update footer links

--- components/options/footer.svelte
+++ components/options/footer.svelte
@@ -1,4 +1,17 @@
-<div class="max-w-3xl mx-auto text-center made-tommy mt-5">
+<script lang="ts">
+  import VindLogo from '~components/vind-logo.svelte'
+</script>
+
+<div
+  class="max-w-3xl mx-auto text-center made-tommy mt-5 flex flex-col gap-3 justify-center items-center">
+  <a href="https://vind-works.io" class="!mix-blend-normal" target="_blank">
+    <VindLogo size="2em" />
+  </a>
+  <div class="flex gap-5 justify-center items-center">
+    <a href="mailto:support@vind-works.io" target="_blank">Help</a>
+    <a href="https://github.com/JoacoEsteban/vind/issues" target="_blank"
+      >Report a bug</a>
+  </div>
   <code>
     With 🧠 + 🩷 by
     <b>
