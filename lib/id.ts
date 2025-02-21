2e0507808843528df6ff3eeb572e464d472ff8de
Joaquin Esteban <dev@joacoesteban.com>
2025-02-21 12:13:32 -0300

    FEAT, implement test-id class

--- lib/id.ts
+++ lib/id.ts
@@ -1,3 +1,16 @@
 export function generateId() {
   return Math.random().toString(36).substring(2)
 }
+
+export function stringToHash(str: string): number {
+  let h1 = 0xdeadbeef,
+    h2 = 0x41c6ce57
+  for (let i = 0; i < str.length; i++) {
+    const ch = str.charCodeAt(i)
+    h1 = Math.imul(h1 ^ ch, 2654435761)
+    h2 = Math.imul(h2 ^ ch, 1597334677)
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507)
+  h2 = Math.imul(h2 ^ (h2 >>> 13), 3266489909)
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0)
+}
